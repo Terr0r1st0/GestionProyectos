dd5020f945b2648f488e3ddf1686b58c474878c5
Terr0r1st0 <ivandavidvargasleon@gmail.com>
2024-06-06 10:21:05 -0500

    Update target

---
+++ target/classes/import.sql
@@ -0,0 +1,13 @@
+INSERT INTO administrador (usuario, contrasena) VALUES('admin', 'admin');
+
+INSERT INTO docente (cedula_docente, contrasena, correo, nombre, telefono, usuario, rol) VALUES (1111,'123','pedro@gmail.com','Pedro Pacheco','3120001111','Pedro','Director');
+INSERT INTO docente (cedula_docente, contrasena, correo, nombre, telefono, usuario, rol) VALUES (1122,'123','fabiou@gmail.com','Fabi Ou','3120001122','Fabi','Director');
+
+INSERT INTO docente (cedula_docente, contrasena, correo, nombre, telefono, usuario, rol) VALUES (2222,'123','manueh@gmail.com','Manueh Rondoh','3120002222','Manueh','Evaluador');
+INSERT INTO docente (cedula_docente, contrasena, correo, nombre, telefono, usuario, rol) VALUES (2233,'123','mbappe@gmail.com','Kylian Mbappe','3120002223','Kylian','Evaluador');
+
+INSERT INTO coordinador (cedula_coordinador, contrasena, correo, nombre, telefono, usuario) VALUES (9999,'123','eladio@gmail.com','Eladio Carrion','3208887777','Eladio');
+
+INSERT INTO estudiante (cedula_estudiante, contrasena, correo, nombre, telefono, usuario) VALUES (1005,'123','ivan@gmail.com','Ivan Vargas','3150008558','Ivan');
+INSERT INTO estudiante (cedula_estudiante, contrasena, correo, nombre, telefono, usuario) VALUES (1006,'123','david@gmail.com','David Leon','3158558007','David');
+INSERT INTO estudiante (cedula_estudiante, contrasena, correo, nombre, telefono, usuario) VALUES (1007,'123','jr10@gmail.com','James Rodriguez','315562558','James');
